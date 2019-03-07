2f1c1bec953bb0b12547d79edf4ebcd10c2beae1
David Reeves <djreeves2005@gmail.com>
2019-03-07 18:23:02 -0500

    [+] Compare

--- The7GATESArchive/Controllers/UserController.cs
+++ The7GATESArchive/Controllers/UserController.cs
@@ -123,8 +123,169 @@ namespace Gateway.Controllers
             return View(users.ToPagedList(pageNumber, pageSize));
         }
 
+        public ActionResult Options(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.KeySortParm = sortOrder == "Key" ? "key_desc" : "Key";
+            ViewBag.NameSortParm = sortOrder == "Name" ? "name_desc" : "Name";
+            ViewBag.RankSortParm = sortOrder == "Rank" ? "rank_desc" : "Rank";
+
+            //First filter the results from the database
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            // See if there's any sort order.
+            if (sortOrder != null)
+            {
+                sortOrder = sortOrder.ToLower();
+            }
+
+            IQueryable<User> users;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                searchString = searchString.ToLower();
+
+                switch (sortOrder)
+                {
+                    case "key_desc":
+                        users = from s in db.Users.Where(u => u.Username.ToLower().Contains(searchString))
+                                .OrderByDescending(u => u.Keys).ThenBy(u => u.TimeForAllGates)
+                                select s;
+                        break;
+                    case "key":
+                        users = from s in db.Users.Where(u => u.Username.ToLower().Contains(searchString))
+                                .OrderBy(u => u.Keys).ThenBy(u => u.TimeForAllGates)
+                                select s;
+                        break;
+                    case "name_desc":
+                        users = from s in db.Users.Where(u => u.Username.ToLower().Contains(searchString))
+                                .OrderByDescending(u => u.Username)
+                                select s;
+                        break;
+                    case "name":
+                        users = from s in db.Users.Where(u => u.Username.ToLower().Contains(searchString))
+                                .OrderBy(u => u.Username)
+                                select s;
+                        break;
+                    case "rank_desc":
+                        users = from s in db.Users.Where(u => u.Username.ToLower().Contains(searchString))
+                                .OrderByDescending(u => u.Rank)
+                                select s;
+                        break;
+                    default:
+                        users = from s in db.Users.Where(u => u.Username.ToLower().Contains(searchString))
+                                .OrderBy(u => u.Rank).ThenBy(u => u.TimeForAllGates)
+                                select s;
+                        break;
+                }
+            }
+            else
+            {
+                switch (sortOrder)
+                {
+                    case "key_desc":
+                        users = from s in db.Users
+                                .OrderByDescending(u => u.Keys).ThenBy(u => u.TimeForAllGates)
+                                select s;
+                        break;
+                    case "key":
+                        users = from s in db.Users
+                                .OrderBy(u => u.Keys).ThenBy(u => u.TimeForAllGates)
+                                select s;
+                        break;
+                    case "name_desc":
+                        users = from s in db.Users
+                                .OrderByDescending(u => u.Username)
+                                select s;
+                        break;
+                    case "name":
+                        users = from s in db.Users
+                                .OrderBy(u => u.Username)
+                                select s;
+                        break;
+                    case "rank_desc":
+                        users = from s in db.Users
+                                .OrderByDescending(u => u.Rank)
+                                select s;
+                        break;
+                    default:
+                        users = from s in db.Users
+                                .OrderBy(u => u.Rank).ThenBy(u => u.TimeForAllGates)
+                                select s;
+                        break;
+                }
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            int pageSize = 100;
+            int pageNumber = (page ?? 1);
+            return View(users.ToPagedList(pageNumber, pageSize));
+        }
         // GET: Students/Details/5
         public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var user = db.Users.Find(id);
+            var usergates = db.UserGates.Where(u => u.UserID == id);
+            var UserViewModel = new UserViewModel();
+            UserViewModel.ID = user.ID;
+            UserViewModel.TimeForAllGates = user.TimeForAllGates;
+            UserViewModel.Keys = user.Keys;
+            UserViewModel.UserGates = usergates.ToList();
+            UserViewModel.Username = user.Username;
+            UserViewModel.Rank = user.Rank;
+            UserViewModel.Percentile = user.Percentile;
+            UserViewModel.PrizeStatus = user.PrizeStatus;
+            UserViewModel.Insight1 = user.Insight1;
+            UserViewModel.Insight2 = user.Insight2;
+
+            return View(UserViewModel);
+        }
+        public ActionResult Compare(Guid? id, Guid? id2)
+        {
+            if (id == null || id2 == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var user = db.Users.Find(id);
+            var usergates = db.UserGates.Where(u => u.UserID == id);
+            var user2 = db.Users.Find(id2);
+            var usergates2 = db.UserGates.Where(u => u.UserID == id2);
+            var UserViewModel = new UserViewModel();
+            UserViewModel.ID = user.ID;
+            UserViewModel.TimeForAllGates = user.TimeForAllGates;
+            UserViewModel.Keys = user.Keys;
+            UserViewModel.UserGates = usergates.ToList();
+            UserViewModel.Username = user.Username;
+            UserViewModel.Rank = user.Rank;
+            UserViewModel.Percentile = user.Percentile;
+            UserViewModel.PrizeStatus = user.PrizeStatus;
+            UserViewModel.Insight1 = user.Insight1;
+            UserViewModel.Insight2 = user.Insight2;
+            UserViewModel.ID2 = user2.ID;
+            UserViewModel.TimeForAllGates2 = user2.TimeForAllGates;
+            UserViewModel.Keys2 = user2.Keys;
+            UserViewModel.UserGates2 = usergates2.ToList();
+            UserViewModel.Username2 = user2.Username;
+            UserViewModel.Rank2 = user2.Rank;
+            UserViewModel.Percentile2 = user2.Percentile;
+            UserViewModel.PrizeStatus2 = user2.PrizeStatus;
+            UserViewModel.Insight12 = user2.Insight1;
+            UserViewModel.Insight22 = user2.Insight2;
+
+            return View(UserViewModel);
+        }
+        public ActionResult Insights(Guid? id)
         {
             if (id == null)
             {
